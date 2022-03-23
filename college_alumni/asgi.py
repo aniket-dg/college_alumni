233b32d1a8c2fac3a46967b0b09fcbde2ae9414c
Aniket <aniket.dg25@gmail.com>
2022-03-23 07:41:43 +0530

    deployment updates 23 march

--- college_alumni/asgi.py
+++ college_alumni/asgi.py
@@ -1,10 +1,14 @@
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'college_alumni.settings')
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'college_alumni.settings')
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
